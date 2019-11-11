97ec8145677b4d3aaced933893e71c9907e398fa
Ayose <ayosec@gmail.com>
2019-11-11 17:06:38 +0000

    Use local DNS

--- run.sh
+++ run.sh
@@ -7,6 +7,7 @@ GWIP="$(docker network inspect bridge -f '{{ range .IPAM.Config }}{{ .Gateway }}
 docker run -ti -u sip --rm         \
   -v /tmp/.X11-unix:/tmp/.X11-unix \
   -w /home/sip                     \
+  --dns "$GWIP"                    \
   -e "DISPLAY=$DISPLAY"            \
   -e "PULSE_SERVER=tcp:$GWIP"      \
   sipclient "$@"
