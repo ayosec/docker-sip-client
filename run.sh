c830005507986597d11216e0ce5ff60f60696d0e
Ayose <ayosec@gmail.com>
2020-06-15 19:30:50 +0100

    Share pulseaudio socket

--- run.sh
+++ run.sh
@@ -4,10 +4,21 @@ set -xe
 
 GWIP="$(docker network inspect bridge -f '{{ range .IPAM.Config }}{{ .Gateway }}{{ end }}')"
 
-docker run -ti -u sip --rm         \
-  -v /tmp/.X11-unix:/tmp/.X11-unix \
-  -w /home/sip                     \
-  --dns "$GWIP"                    \
-  -e "DISPLAY=$DISPLAY"            \
-  -e "PULSE_SERVER=tcp:$GWIP"      \
+PULSE_DIR="$XDG_RUNTIME_DIR"
+
+if [ ! -d "$PULSE_DIR" ]
+then
+  echo "Missing $PULSE_DIR"
+  exit 1
+fi
+
+docker run -ti -u sip --rm              \
+  -v /tmp/.X11-unix:/tmp/.X11-unix      \
+  -w /home/sip                          \
+  --dns "$GWIP"                         \
+  -v "$PULSE_DIR:$PULSE_DIR"            \
+  -e "XDG_RUNTIME_DIR=$XDG_RUNTIME_DIR" \
+  -e "DISPLAY=$DISPLAY"                 \
+  -e "GDK_SCALE=$GDK_SCALE"             \
+  -e "QT_SCALE_FACTOR=$QT_SCALE_FACTOR" \
   sipclient "$@"
